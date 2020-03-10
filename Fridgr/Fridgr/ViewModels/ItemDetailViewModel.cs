1befb54df0a352c66f78ba37c22989a34d1d781e
lauren diaz <lauren@laurens-MacBook-Pro.local>
2020-03-10 16:28:50 -0400

    initial project generation

---
+++ Fridgr/Fridgr/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Fridgr.Models;
+
+namespace Fridgr.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
